03ec45e00c01c0d9ee12a2f640f2fd68ef84a2f7
andrejfaria <ajcf10@gmail.com>
2023-07-16 22:36:45 +0100

    buil

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
-
-
+#!/usr/bin/env node
 import fse from 'fs-extra';
 import inquirer from 'inquirer';
-import path from 'path';
+import path, { join } from 'path';
+import { argv } from 'node:process';
 // import { dirname } from 'path';
 
 import { fileURLToPath } from 'url';
 
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
-// console.log(path.join(__dirname, "templates"));
-
+const __dirname = path.dirname(__filename);
 async function main() {
+
+  let targetdir = ''
+  if (!argv[2]) {
+    targetdir = process.cwd();
+  }
+  targetdir = argv[2];
+
+  console.log(argv[2])
+  console.log('--------->', process.cwd()
+    , 'process')
+
+  console.log('--------->', targetdir
+    , 'target')
+  const destination = path.join(process.cwd(), targetdir)
 
 
   const { project } = await inquirer.prompt([
@@ -21,13 +33,17 @@ async function main() {
       name: "project",
       choices: await fse.readdir(path.join(__dirname, "templates")),
     }])
-  console.log(process.argv)
-  console.log(project)
+
+
   await fse.copy(
-    path.join(__dirname, "./templates", project),
-    path.join(__dirname, "../", project)
-  )
-  // console.log(path.join(__dirname, "./templates", project));
+    // path.join(__dirname, "./templates", project),
+    // path.join(__dirname, "../", project)
+    path.join(__dirname, "./templates", project), destination)
 }
 
+
+
+
 main();
+
+
